13c42b1eff48f06d9ddf3e3f3221447404ab2053
Dawid <dkubik@st.swps.edu.pl>
2026-01-28 04:56:12 +0100

    Lab 3 finito

--- gatunki_zagrozone/morze_srodziemne/gatunki_zagrozone_basenu_morza_srodziemnego/admin.py
+++ gatunki_zagrozone/morze_srodziemne/gatunki_zagrozone_basenu_morza_srodziemnego/admin.py
@@ -1,3 +1,41 @@
-from django.contrib import admin
+#Część kody wpełni wygenerowna przez chat
 
-# Register your models here.
+from django.contrib import admin
+from .models import Continent, Country, Species
+
+@admin.register(Continent)
+class ContinentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'continent', 'domain_code') # Kolumny w tabeli
+    list_filter = ('continent',) # Filtr po prawej stronie
+    search_fields = ('name',) # Wyszukiwarka
+
+@admin.register(Species)
+class SpeciesAdmin(admin.ModelAdmin):
+    # Co widzimy na liście gatunków
+    list_display = ('name_pl', 'name_lat', 'category_ckgz', 'is_endemic')
+    # Filtry, które ułatwią Ci życie
+    list_filter = ('category_ckgz', 'is_endemic', 'protection_actions')
+    # Wyszukiwanie po nazwach polskich i łacińskich
+    search_fields = ('name_pl', 'name_lat')
+    # Grupowanie pól w formularzu edycji (opcjonalne, ale bardzo czytelne)
+    fieldsets = (
+        ('Podstawowe informacje', {
+            'fields': ('name_pl', 'name_lat', 'category_ckgz')
+        }),
+        ('Systematyka', {
+            'fields': ('kingdom', 'phylum', 'species_class', 'order', 'family', 'genus')
+        }),
+        ('Ochrona i status', {
+            'fields': ('protection_actions', 'protection_intensity', 'protection_organizations', 'is_endemic', 'is_mainstream')
+        }),
+        ('Opisy', {
+            'fields': ('description', 'appearance', 'habitat_and_diet')
+        }),
+        ('Lokalizacja', {
+            'fields': ('countries',)
+        }),
+    )
--- gatunki_zagrozone/morze_srodziemne/morze_srodziemne_serce/settings.py
+++ gatunki_zagrozone/morze_srodziemne/morze_srodziemne_serce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gatunki_zagrozone_basenu_morza_srodziemnego',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -115,3 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# to jest do zdięc, odhashuj
+#import os
+#
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
